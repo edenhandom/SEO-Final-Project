ee7b32fd889c6d3c4a99436339fa26aa6ac3fb83
Abigail Sheldon <pocketpuppy@Abigails-MacBook-Pro.local>
2024-07-18 10:55:28 -0400

    updated main app file to work with classes

--- app.py
+++ app.py
@@ -1,121 +1,72 @@
-
 import os
 import json
-import openai
 import requests
-from openai import OpenAI
-import re
-import sys
-import io
-import base64
-import logging
-
-#from spotipy import Spotify
-#from spotipy.oauth2 import SpotifyOAuth
-
 from flask import (Flask, request, redirect, session, 
                    url_for, render_template, flash, make_response)
 
+''''
+Import our Classes
+'''
+from user_session import UserSession
+from spotify_client import SpotifyClient
+from openai_client import OpenAIClient
 
 '''
 Import our modules
 '''
 from user_form import UserForm
 from pull_playlist import *
+from insights_module import *
 
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
 
 app = Flask(__name__) # static_folder="static", static_url_path=""
-app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SECRET_KEY'] = os.urandom(64)   # generate random session key
 
 
-# FOR SPOTIFY API
-#CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
-CLIENT_ID = 'ad91a46157df4ba080456f92c7a74ef8'
-#CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
-CLIENT_SECRET= '9d4140d511c64467a582b075b990cbfe'
-
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-BASE_URL = 'https://api.spotify.com/v1/'
-
-redirect_uri = 'http://localhost:3000/callback'
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+USER_KEY = os.environ.get("USER_KEY")
 SCOPE = 'playlist-read-private user-library-read user-read-recently-played'
+REDIECT_URI = 'http://localhost:3000/callback'
 
+# Connect to the spotify API
+spotify_client = SpotifyClient(CLIENT_ID, CLIENT_SECRET, REDIECT_URI, SCOPE)
+# Connect to OpenAI API
+openai_client = OpenAIClient(USER_KEY)
+# Start user session
+user_session = UserSession()
 
-# FOR OPEN AI API
-#USER_KEY = os.environ.get("USER_KEY")
-USER_KEY = ''
-client = OpenAI(api_key=USER_KEY)
-
-
+# Route to start the authorization process
 @app.route('/')
 def login():
-    auth_url = f"https://accounts.spotify.com/authorize?response_type=code&client_id={CLIENT_ID}&scope={SCOPE}&redirect_uri={redirect_uri}&show_dialog=True"
-    return redirect(auth_url)
+    return redirect(spotify_client.get_auth_url())
 
-
+# Callback route to handle the redirect from Spotify
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
-    auth_token_url = 'https://accounts.spotify.com/api/token'
-    auth_token_data = {
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': redirect_uri,
-    }
-    auth_token_headers = {
-        'Authorization': 'Basic ' + base64.b64encode((CLIENT_ID + ':' + CLIENT_SECRET).encode()).decode()
-    }
-    response = requests.post(auth_token_url, data=auth_token_data, headers=auth_token_headers)
-    response_data = response.json()
-    session['token'] = response_data['access_token']
+    spotify_client.get_token(code)
+    user_session.set_token(spotify_client.token)
     return redirect(url_for('home'))
+
+@app.route('/home')
+def home():
+  return render_template('home.html')
+
 
 @app.route('/mood')
 def mood():
-    token = session.get('token')
-    if not token:
-        return redirect(url_for('home'))
-    headers = {
-        'Authorization': f'Bearer {token}'
-    }
-    
-    profile_url = 'https://api.spotify.com/v1/me'
-
-    try: 
-        profile_response = requests.get(profile_url, headers=headers)
-        profile_data = profile_response.json()
-
-    except requests.exceptions.JSONDecodeError:
-        print(profile_response.status_code)
-        print("HTTP RESPONSE")
-
-    # profile_data = profile_response.json()
-    
-    playlists_url = 'https://api.spotify.com/v1/me/playlists'
-    playlists_response = requests.get(playlists_url, headers=headers)
-    playlists_data = playlists_response.json()
-
-    recent_tracks_url = 'https://api.spotify.com/v1/me/player/recently-played'
-    recent_tracks_response = requests.get(recent_tracks_url, headers=headers)
-    recent_tracks_data = recent_tracks_response.json()
-
-    tracks_artists = {
-        track['track']['name']: ', '.join([artist['name'] for artist in track['track']['artists']])
-        for track in recent_tracks_data['items']
-    }
-    tracks_artists_str = '. '.join([f"{track}: {artist}" for track, artist in tracks_artists.items()])
+    # Dictioanry of songs and artists
+    tracks_artists = {}
+    # Readable string of playlist songs
+    tracks_artists_str = ""
     prompt = (
-            f"Give me a mood (like an emotion) " 
+            f"Give me a mood (an emotion) " 
             f"based on my favorite recent songs: "
             f"{tracks_artists_str}. "
             f"Please give me a one or two word mood."
             )
-
-    response = get_chat_response(prompt)
-
+    response = openai_client.get_chat_response(prompt)
     return render_template('mood.html', top_tracks = tracks_artists, response = response)
 
 
@@ -145,9 +96,6 @@ def top_artists():
     return f"Top Artists: {json.dumps(artist_names, indent=4)}"
 
 
-@app.route('/home')
-def home():
-  return render_template('home.html')
 
 
 # Adding User Form page to website
@@ -189,17 +137,17 @@ def submit_page():
             f"Format like: 'Song1'\n 'Song2'\n...'"
             )
 
-        recommendations = get_chat_response(prompt)
-        song_list = extract_song_titles(recommendations)
+        recommendations = openai_client.get_chat_response(prompt)
+        song_list = spotify_client.extract_song_titles(recommendations)
 
         song_with_preview = []
         for song in song_list:
-            track_id, preview_url, artist_name = get_song_data(song)
+            track_id, preview_url, artist_name = spotify_client.get_song_data(song)
             if track_id and preview_url and artist_name:
                song_with_preview.append({'song': song, 'artist': artist_name, 'track_id': track_id, 'preview_url': preview_url})
 
         for song in song_with_preview:
-            song['link'] = get_song_link(song['track_id'])
+            song['link'] = spotify_client.get_song_link(song['track_id'])
 
         return render_template('submit_page.html', 
                                title='Submitted Data', 
@@ -210,7 +158,7 @@ def submit_page():
         return redirect(url_for('user_form'))
 
               
-@ app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/insights', methods=['GET', 'POST'])
 def insights():
     if request.method == 'POST':
@@ -219,7 +167,7 @@ def insights():
         print(f"Received playlist URL: {playlist_url}")
         
         if playlist_url:
-            track_artist = get_playlist_data(playlist_url)
+            track_artist = spotify_client.get_public_playlist_data(playlist_url)
             
             if track_artist:
                 tracks_artists_str = '. '.join([f"{track}: {artist}" for track, artist in track_artist.items()])
@@ -232,7 +180,7 @@ def insights():
                           f'Be concise and format with line breaks between '
                           f'each insight.')
                 
-                chat_response = get_chat_response(prompt)
+                chat_response = openai_client.get_chat_response(prompt)
                 chat_response = chat_response.replace('\n', '<br>')
 
 
