ee5c9ce563828fbbaddf3f0d44dffaab520c5810
Abigail Sheldon <pocketpuppy@Abigails-MacBook-Pro.local>
2024-07-22 18:31:45 -0400

    added styling for submit page & the db page for user form, made a small tweak to the about page styling

--- user_form.py
+++ user_form.py
@@ -6,15 +6,15 @@ class UserForm(FlaskForm):
     star_sign = StringField(
         'Star Sign', 
         validators=[DataRequired()],
-        render_kw={"placeholder": "✴ Enter your astrology sign: "}
+        render_kw={"placeholder": "Enter your astrology sign: "}
         )
     personality_traits = TextAreaField(
         'Personality Traits', 
         validators=[DataRequired()],
         render_kw={
           "placeholder": (
-              "✰ Enter some personality traits!"
-              "ex. silly, mellow, creative, etc."
+              "Enter some personality traits! "
+              "ex. silly, creative, etc."
             )
         }
     )
@@ -22,28 +22,28 @@ class UserForm(FlaskForm):
     fav_genre1 = StringField(
         'Favorite Genre 1', 
         validators=[DataRequired()],
-        render_kw={"placeholder": "✴ Enter a favorite genre: "}
+        render_kw={"placeholder": "Enter a favorite genre: "}
         )
     optional_field1 = StringField(
         'Additional Info 1', validators=[Optional()],
-        render_kw={"placeholder": "✰ Example song/artist: "}
+        render_kw={"placeholder": "✰ Example song/artist "}
         )
     fav_genre2 = StringField(
         'Favorite Genre 2', validators=[DataRequired()],
-        render_kw={"placeholder": "✴ Enter a favorite genre: "}
+        render_kw={"placeholder": "Enter a favorite genre: "}
         )
     optional_field2 = StringField(
         'Additional Info 2', validators=[Optional()],
-        render_kw={"placeholder": "✰ Example song/artist: "}
+        render_kw={"placeholder": "✰ Example song/artist "}
         )
     fav_genre3 = StringField(
         'Favorite Genre 3', validators=[DataRequired()],
-        render_kw={"placeholder": "✴ Enter a favorite genre: "}
+        render_kw={"placeholder": "Enter a favorite genre: "}
         )
     optional_field3 = StringField(
         'Additional Info 3', 
         validators=[Optional()],
-        render_kw={"placeholder": "✰ Example song/artist: "}
+        render_kw={"placeholder": "✰ Example song/artist "}
         )
     
     include_history = SelectField(
