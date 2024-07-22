b6539eff31f5495088d382452217a18b29559f09
Abigail Sheldon <pocketpuppy@Abigails-MacBook-Pro.local>
2024-07-22 14:44:29 -0400

    fixed some typos in app file

--- app.py
+++ app.py
@@ -140,15 +140,16 @@ def mood():
             user_mood = request.form.get('user_mood')
             if user_mood:
                 prompt = (
-                    f"Give me a playlist of songs
-                    that match the mood '{user_mood}'. "
-                    f"Here are some of my favorite
-                    recent songs: {tracks_artists_str}. "
-                    f"Do not give me any songs
-                    from my recent songs."
-                    f"Please list each song on a
-                    new line, song title only in quotes. "
-                    f"Format like: 'Song1'\n 'Song2'\n...")
+                    f"Give me a playlist of songs "
+                    f"that match the mood '{user_mood}'. "
+                    f"Here are some of my favorite "
+                    f"recent songs: {tracks_artists_str}. "
+                    f"Do not give me any songs "
+                    f"from my recent songs."
+                    f"Please list each song on a "
+                    f"new line, song title only in quotes. "
+                    f"Format like: 'Song1'\n 'Song2'\n..."
+                    )
 
                 raw_response = openai_client.get_chat_response(prompt)
                 song_list = spotify_client.extract_song_titles(raw_response)
@@ -251,8 +252,8 @@ def submit_page():
                 for track in recent_tracks_data['items']
             }
             tracks_artists_str = '. '.join(
-                [f"{track}:
-                 {artist}" for track, artist
+                [f"{track}: "
+                 f"{artist}" for track, artist
                  in tracks_artists.items()])
 
             prompt = (
