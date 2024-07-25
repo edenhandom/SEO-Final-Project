6ab1194071f62db15369947946d8b0af909fdc26
edenhandom <edennie@Edens-MacBook-Pro.local>
2024-07-24 21:28:19 -0700

    final fixes

--- util/spotify_client.py
+++ util/spotify_client.py
@@ -3,6 +3,7 @@ import base64
 import re
 from flask import session, redirect, url_for, render_template
 from pprint import pprint
+import pandas as pd
 import json
 
 class SpotifyClient:
@@ -74,7 +75,6 @@ class SpotifyClient:
     def get_headers(self):
         return {'Authorization': f'Bearer {self.token}'}
 
-
     def get_profile_data(self):
         profile_url = f"{self.BASE_URL}me"
         response = requests.get(profile_url, headers=self.get_headers())
@@ -84,7 +84,6 @@ class SpotifyClient:
             print("Failed to retrieve profile data")
             print("Status Code: ", response.status_code)
             return None
-
 
     def get_user_playlists(self):
         playlists_url = f"{self.BASE_URL}me/playlists"
@@ -119,6 +118,42 @@ class SpotifyClient:
             return None
 
 
+    # Use if you only have playlist ID of playlist
+    def get_playlist_items_with_id(self, playlist_id):
+        # Authenticate with Spotify API
+        auth_response_data = self.connectSpotifyAPI()
+
+        if 'access_token' in auth_response_data:
+            access_token = auth_response_data['access_token']
+            headers = {'Authorization': f'Bearer {access_token}'}
+
+            # Request to get playlist tracks
+            response = requests.get(
+                f'{self.BASE_URL}playlists/{playlist_id}/tracks', 
+                headers=headers
+            )
+
+            if response.status_code == 200:
+                playlist_data = response.json()
+
+                # Extract track names and artist names
+                tracks_artists = {
+                    track['track']['name']: ', '.join(
+                        [artist['name'] for artist in track['track']['artists']]
+                    )
+                    for track in playlist_data['items']
+                }
+
+                return tracks_artists
+            else:
+                print("Failed to retrieve playlist items")
+                print("Status Code:", response.status_code)
+                return None
+        else:
+            print("Failed to authenticate with Spotify API")
+            return None
+
+
     def get_public_playlist_data(self, playlist_url):
         auth_response_data = self.connectSpotifyAPI()
 
@@ -126,7 +161,7 @@ class SpotifyClient:
             access_token = auth_response_data['access_token']
             headers = {'Authorization': f'Bearer {access_token}'}
 
-            playlist_id = re.findall(r'playlist/([a-zA-Z0-9]+)', playlist_url)
+            playlist_id = re.findall(r'playlists/([a-zA-Z0-9]+)', playlist_url)
 
             if playlist_id:
                 playlist_id = playlist_id[0]
@@ -155,7 +190,7 @@ class SpotifyClient:
         else:
             print("Failed to authenticate with Spotify API")
             return None
-
+        
 
     def extract_song_titles(self, input_string):
         pattern = r'"([^"]+)"'
@@ -202,6 +237,28 @@ class SpotifyClient:
                     return track_id, preview_url, artist_name
 
         return None, None, None
+
+    #get playlist detail
+    def get_playlist_details(self,playlist_id):
+        auth_response_data = self.connectSpotifyAPI()
+
+        if 'access_token' in auth_response_data:
+            access_token = auth_response_data['access_token']
+            headers = {'Authorization': f'Bearer {access_token}'}
+            playlistdetails_url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
+            response = requests.get(playlistdetails_url, headers=headers)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                print("Failed to retrieve playlist details")
+                print("Status Code: ", response.status_code)
+                return None
+
+
+    # track ids to uris
+    def convert_ids_to_uris(self, track_ids):
+        base_uri = "spotify:track:"
+        return [base_uri + track_id for track_id in track_ids]
 
     #need
     def get_audio_features(self, track_ids_str):
@@ -314,3 +371,20 @@ class SpotifyClient:
         else:
             raise Exception(f"Error {response.status_code}: {response.json()}")
    
+    # Convert a json of user's playlists into a list, then a pandas df
+    def json_to_db(self, playlist_json):
+        playlists = []  # {id, name}
+
+        for playlist in playlist_json["items"]:
+            playlists.append({
+                'playlist_href': playlist['href'],
+                'playlist_name': playlist['name'],
+                # Include other relevant fields
+            })
+
+        # convert to pandas DF
+        #playlists_df = pd.DataFrame(list(playlists.items()), columns=['id', 'name'])
+        return pd.DataFrame(playlists)
+        #return playlists_df
+
+    # convert and store in db
